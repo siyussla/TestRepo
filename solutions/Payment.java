9f5c1bd566bfd9dd36c315529d8b550437338ee5
siyussla <92722334+siyussla@users.noreply.github.com>
2022-06-15 16:01:25 +0800

    Update Payment.java

--- solutions/Payment.java
+++ solutions/Payment.java
@@ -1,132 +1,162 @@
-package Payment;
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
 
-import java.io.BufferedInputStream;
-import java.text.DecimalFormat;
-import java.text.NumberFormat;
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Scanner;
+import java.util.*;
 
-public class Payment {
+/**
+ *
+ * @author nisyaqanita
+ */
+public class Payment implements Comparable<Payment>{
+    
+    // variable block
+    private Long oldEpoch;
+    private Long newEpoch;
+    private Long newEpochHolder;
+    private String txnId;
+    private String tier;
+    
+    final String PLATINUM = "PLATINUM";
+    final String GOLD = "GOLD";
+    final String SILVER = "SILVER";
+    
+    static PriorityQueue<Payment> meowsPQ = new PriorityQueue<>();
+    private static Stack<Long> stack = new Stack<>();
+    // end of variale block
+    
+    public Payment(Long oldEpoch, String txnId, String tier) {
+        if (tier.equalsIgnoreCase(PLATINUM)) {
+            this.newEpoch = oldEpoch - 3000;
+        }
+        else if (tier.equalsIgnoreCase(GOLD)) {
+            this.newEpoch = oldEpoch - 2000;
+        }
+        else if (tier.equalsIgnoreCase(SILVER)) {
+            this.newEpoch = oldEpoch - 1000;
+        } 
+        else {
+            this.newEpoch = oldEpoch - 0;
+        }
+        
+        newEpochHolder = this.newEpoch;
+        
+        if (!stack.isEmpty()) {
+            if (stack.contains(newEpochHolder)) {
+                newEpochHolder += 1L;
+            }
+        }
+        if (stack.size() == 1800) {
+            stack.clear();
+        }
+        
+        this.newEpoch = newEpochHolder;
+        stack.push(this.newEpoch);
 
-    static Queue<String> Platinum = new Queue<>();
-    static Queue<String> Gold = new Queue<>();
-    static Queue<String> Silver = new Queue<>();
-    static Queue<String> Bronze = new Queue<>();
-    static ArrayList<Character> check = new ArrayList<>();
+        this.oldEpoch = oldEpoch;
+        this.txnId = txnId;
+        this.tier = tier;
+    }
 
-    public static void main(String[] args) {
+    public Long getOldEpoch() {return oldEpoch;}
+    
+    public Long getNewEpoch() {return newEpoch;}
 
-        Scanner sc = new Scanner(new BufferedInputStream(System.in));
+    public String getTxnId() {return txnId;}
+
+    public String getTier() {return tier;}
+    
+    @Override 
+    public int compareTo(Payment otherTransaction) {
+        return this.getNewEpoch().compareTo(otherTransaction.newEpoch);
+    }
+    
+    @Override
+    public String toString() {
+        return this.getTxnId();
+    }
+    
+    public static String toStr(PriorityQueue<Payment> q) {
+        if (stack.size() == 150) {
+
+        }
+        String result = "";
         int i = 0;
-
-        while (true) {
-            System.out.print("INPUT :");
-            String input = sc.nextLine();
-
-            if (input.equals("END")) {
-
-                System.out.println("REBOOT");
-                break;
-            } else if (input.equals("CLEAR")) {
-                print(i);
-            } else {
-                String[] words = input.split(" ");
-                check.add(words[0].charAt(9));
-                if (i == 0) {
-
-                    if (words[2].equals("PLATINUM")) {
-                        Platinum.enqueue(words[1]);
-                    } else if (words[2].equals("GOLD")) {
-                        Gold.enqueue(words[1]);
-                    } else if (words[2].equals("SILVER")) {
-                        Silver.enqueue(words[1]);
-                    } else if (words[2].equals("BRONZE")) {
-                        Bronze.enqueue(words[1]);
-                    }
-
+        while(!q.isEmpty() && i < 100){
+            result += q.poll() + " ";
+            i++;
+        }
+        return result.trim();
+    }
+    
+    public static void main(String[] args) {
+        // variables block
+        final String EXIT = "EXIT";
+        final String REBOOT = "REBOOT";
+        
+        Long epoch;
+        String txnId, tier;
+        
+        int diff = 0;
+        Long lastAddedEpoch = 0L;
+        boolean pass = false;
+        // end of variale block
+                
+        Scanner sc = new Scanner(System.in);
+        
+        while (sc.hasNextLine()) {
+            try {
+                String in = sc.nextLine();
+                in = in.trim();
+                
+                if (in.equalsIgnoreCase(EXIT)) {
+                    break;
+                } else if (in.equalsIgnoreCase(REBOOT)) {
+                    meowsPQ.clear();
                 } else {
-                    if (check.get(i - 1).equals(check.get(i))) {
-
-                        if (words[2].equals("PLATINUM")) {
-                            Platinum.enqueue(words[1]);
-                        } else if (words[2].equals("GOLD")) {
-                            Gold.enqueue(words[1]);
-                        } else if (words[2].equals("SILVER")) {
-                            Silver.enqueue(words[1]);
-                        } else if (words[2].equals("BRONZE")) {
-                            Bronze.enqueue(words[1]);
-                        }
+                    String [] transaction = in.split("\\s+", 3);
+                    epoch = Long.valueOf(transaction[0]);
+                    txnId = transaction[1];
+                    tier = transaction[2];
+                    
+                    if (!meowsPQ.isEmpty()) {
+                        lastAddedEpoch /= 1000;
+                        
+                        Long epochTemp = epoch;
+                        epochTemp /= 1000;
+                        
+                        diff = epochTemp.intValue() - lastAddedEpoch.intValue();
+                        lastAddedEpoch = epoch;
+                        
                     } else {
-
-                        if (words[2].equals("PLATINUM")) {
-                            Platinum.enqueue(words[1]);
-                        } else if (words[2].equals("GOLD")) {
-                            Gold.enqueue(words[1]);
-                        } else if (words[2].equals("SILVER")) {
-                            Silver.enqueue(words[1]);
-                        } else if (words[2].equals("BRONZE")) {
-                            Bronze.enqueue(words[1]);
-                        }
-                        i++;
-                        print(i);
-
-                        System.exit(0);
+                        lastAddedEpoch = epoch;
                     }
-
-                }
-                i++;
+                    
+                    Payment transactionObj = new Payment(epoch, txnId, tier);
+                    meowsPQ.add(transactionObj);
+                    
+                    Long round = lastAddedEpoch % 1000;
+                    if (diff == 1 || pass) { 
+                        if (round.intValue() != 0) {
+                            String ans = toStr(meowsPQ);
+                            System.out.println(ans);
+                            diff = 0;
+                            pass = false;
+                        } else {
+                            pass = true;
+                        }
+                    }  
+                } 
+                
+            } catch (InputMismatchException e) {
+                return;
+            } catch (NumberFormatException e) {
+                return;
+            } catch (ArrayIndexOutOfBoundsException e) {
+                return;
             }
         }
-
-    }
-
-    public static void print(int i) {
-
-        System.out.print("Output :");
-        for (int count = 0; count < i; count++) {
-
-            if (!Platinum.isEmpty()) {
-                System.out.print(Platinum.dequeue() + " ");
-            }
-            if (!Gold.isEmpty()) {
-
-                System.out.print(Gold.dequeue() + " ");
-            }
-            if (!Silver.isEmpty()) {
-
-                System.out.print(Silver.dequeue() + " ");
-            }
-            if (!Bronze.isEmpty()) {
-
-                System.out.print(Bronze.dequeue() + " ");
-            }
-        }
-    }
-
-    public static class Queue<E> {
-
-        private java.util.LinkedList<E> list = new java.util.LinkedList<>();
-
-        public Queue() {
-        }
-
-        public void enqueue(E e) {
-
-            list.addLast(e);
-
-        }
-
-        public E dequeue() {
-
-            return list.removeFirst();
-
-        }
-        public boolean isEmpty() {
-
-            return list.isEmpty();
-
-        }
-    }
-
+        
+    } 
 }
